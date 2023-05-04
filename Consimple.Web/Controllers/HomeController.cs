31575a52230c49b7b943d19243dac61740b20125
NikHusachenko <nikgusachenko@gmail.com>
2023-05-04 11:39:39 +0300

    Добавьте файлы проекта.

---
+++ Consimple.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Consimple.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Index")]
+        public ActionResult<string> Index()
+        {
+            return "Hello, World!";
+        }
+    }
+}
